6ec9e92eae9e67367e035e77e9393d35d29876a4
Nuranggi Hermawan <nuranggihermawan@gmail.com>
2022-09-03 23:51:41 +0700

    Update

--- app/modules/group/group.controller.js
+++ app/modules/group/group.controller.js
@@ -18,57 +18,6 @@ var uniqueCode;
 // VALIDATION
 exports.validate = (method) => {
     switch (method) {
-        case "createUser":
-            return [
-                body('c_first_name').notEmpty().withMessage('c_first_name harus terisi!')
-                .isLength({
-                    max: 32
-                }).withMessage('c_firstname is out of length!'),
-                body('c_last_name').exists().withMessage('c_last_name is required!')
-                .isLength({
-                    max: 32
-                }).withMessage('c_lastname is out of length!'),
-                body('c_group_code').notEmpty().withMessage('c_group_code harus terisi!')
-                .isLength({
-                    max: 8
-                }).withMessage('c_group_code is out of length!'),
-                body('c_email').notEmpty().withMessage('c_email harus terisi!').isEmail().withMessage("Invalid Email format")
-                .isLength({
-                    max: 64
-                }).withMessage('c_email is out of length!')
-            ]
-
-        case "updateUser":
-            return [
-                check("code").notEmpty().withMessage('code harus terisi!'),
-                body('c_first_name').notEmpty().withMessage('c_first_name harus terisi!')
-                .isLength({
-                    max: 32
-                }).withMessage('c_firstname is out of length!'),
-                body('c_last_name').notEmpty().withMessage('c_last_name is required!')
-                .isLength({
-                    max: 32
-                }).withMessage('c_lastname is out of length!'),
-                body('c_group_code').notEmpty().withMessage('c_group_code harus terisi!')
-                .isLength({
-                    max: 8
-                }).withMessage('c_group_code is out of length!'),
-                body('c_email').notEmpty().withMessage('c_email harus terisi!').isEmail().withMessage("Invalid Email format")
-                .isLength({
-                    max: 64
-                }).withMessage('c_email is out of length!')
-            ]
-
-        case "resetPassword":
-            return [
-                check("code").notEmpty().withMessage('code harus terisi!'),
-                body('c_password').exists().withMessage('c_password harus terisi!')
-                .isLength({
-                    max: 32
-                }).withMessage('c_password is out of length!'),
-                body('reset').notEmpty()
-                .isBoolean().withMessage('reset is out of length!'),
-            ]
     
         default:
             break;
